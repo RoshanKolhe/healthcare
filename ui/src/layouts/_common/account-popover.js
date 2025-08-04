294dad123b49688fd6472b6f9a39b84d2accfcf3
abhi <kolheroshan508@gmail.com>
2025-08-04 11:38:15 +0530

    Profile updating as user chnages

--- ui/src/layouts/_common/account-popover.js
+++ ui/src/layouts/_common/account-popover.js
@@ -42,9 +42,7 @@ const OPTIONS = [
 export default function AccountPopover() {
   const router = useRouter();
 
-  const { user } = useMockedUser();
-
-  const { logout } = useAuthContext();
+  const { logout, user } = useAuthContext();
 
   const { enqueueSnackbar } = useSnackbar();
 
@@ -86,7 +84,7 @@ export default function AccountPopover() {
       >
         <Avatar
           src={user?.photoURL}
-          alt={user?.displayName}
+          alt={`${user?.firstName} ${user?.lastName}`}
           sx={{
             width: 36,
             height: 36,
@@ -98,7 +96,7 @@ export default function AccountPopover() {
       <CustomPopover open={popover.open} onClose={popover.onClose} sx={{ width: 200, p: 0 }}>
         <Box sx={{ p: 2, pb: 1.5 }}>
           <Typography variant="subtitle2" noWrap>
-            {user?.displayName}
+            {`${user?.firstName} ${user?.lastName}`}
           </Typography>
 
           <Typography variant="body2" sx={{ color: 'text.secondary' }} noWrap>
